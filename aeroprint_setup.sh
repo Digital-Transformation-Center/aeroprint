560e0a70fd7d103295f56a7ae78df81d1eabf6c9
Ryan Kuederle <ryankuederle@icloud.com>
2024-03-13 13:52:43 -0400

    Test setuo

--- aeroprint_setup.sh
+++ aeroprint_setup.sh
@@ -7,4 +7,4 @@ sudo apt update && install curl -y
 sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 sudo apt update && sudo apt upgrade -y
-sudo apt install ros-foxy-desktop python3-argcomplete
+sudo apt install ros-foxy-desktop python3-argcomplete -y
