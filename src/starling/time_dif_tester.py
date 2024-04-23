1064d0dc6df26e3bcd1ca1344700b5b2dc1f0762
Ryan Kuederle <ryankuederle@icloud.com>
2024-04-23 16:40:24 -0400

    Added max dif

--- src/starling/time_dif_tester.py
+++ src/starling/time_dif_tester.py
@@ -23,15 +23,16 @@ class PCNode(Node):
     )
     self.pose_node = pose_node
     self.pub_rate = 2.0 #2 hz
+    self.max_time_dif = 5000000
     self.last_pub_time = self.get_clock().now().nanoseconds
 
   def callback(self, data):
     current_time = self.get_clock().now().nanoseconds
-    if (current_time - self.last_pub_time) >= (1 / self.pub_rate) * 1e9:
+    pose = self.pose_node.get_pose()
+    pose_time = pose.header.stamp.nanosec
+    pc_time = data.header.stamp.nanosec
+    if (current_time - self.last_pub_time) >= (1 / self.pub_rate) * 1e9 and abs(pose_time - pc_time) <= self.max_time_dif:
         rclpy.spin_once(self.pose_node)
-        pose = self.pose_node.get_pose()
-        pose_time = pose.header.stamp.nanosec
-        pc_time = data.header.stamp.nanosec
         time_dif = abs(pose_time - pc_time) / 1000000000.0
         print(time_dif)
         self.last_pub_time = current_time
