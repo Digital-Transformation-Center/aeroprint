2632db035fc076c19c52b7e617897bff8648e1a9
Ryan Kuederle <ryankuederle@icloud.com>
2024-04-23 14:47:28 -0400

    fixes

--- src/starling/starling/posed_pc_publisher.py
+++ src/starling/starling/posed_pc_publisher.py
@@ -4,7 +4,7 @@ from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Header
 import open3d as o3d
 import numpy as np
-from rclpy.qos import qos_profile_sensor_data, qos_profile_system_default, QoSHistoryPolicy, QoSDurabilityPolicy, QoSReliabilityPolicy
+from rclpy.qos import qos_profile_sensor_data, qos_profile_system_default, QoSHistoryPolicy, QoSDurabilityPolicy, QoSReliabilityPolicy, QoSProfile
 from rclpy.node import Node
 import numpy as np
 import open3d as o3d
@@ -24,44 +24,50 @@ class PCNode(Node):
       PointCloud2, 
       topic, 
       self.callback, 
-      qos_profile
+      qos_profile_sensor_data
     )
     self.publisher = self.create_publisher(
         PointCloud2, "/starling/out/posed_pc", qos_profile_sensor_data
     )
     self.pose_node = pose_node
+    self.pub_rate = 2.0 #2 hz
+    self.last_pub_time = self.get_clock().now().nanoseconds
 
     
 
   def callback(self, data):
-    # Get latest pose data
-    rclpy.spin_once(self.pose_node)
-    pose = self.pose_node.get_pose()
-    pose_time = pose.header.stamp.nanosec
-    pc_time = data.header.stamp.nanosec
-    time_dif = (pose_time - pc_time) / 1000000000.0
-    print(time_dif)
-    # Get position and orientation from pose
-    position = pose.pose.position
-    orientation = pose.pose.orientation
-    # Convert PointCloud2 to numpy and o3d
-    points = np.frombuffer(data.data, dtype=np.float32).reshape(-1, 3)# [:, :3]
-    for i in range(len(points)):
-        points[i] = [points[i][2], -points[i][1], points[i][0]]
-    o3dpc = o3d.geometry.PointCloud()
-    o3dpc.points = o3d.utility.Vector3dVector(points)
+    current_time = self.get_clock().now().nanoseconds
+    if (current_time - self.last_pub_time) >= (1e9 / self.pub_rate):
+      # Get latest pose data
+      rclpy.spin_once(self.pose_node)
+      pose = self.pose_node.get_pose()
+      pose_time = pose.header.stamp.nanosec
+      pc_time = data.header.stamp.nanosec
+      time_dif = (pose_time - pc_time) / 1000000000.0
+      print(time_dif)
+      # Get position and orientation from pose
+      position = pose.pose.position
+      orientation = pose.pose.orientation
+      # Convert PointCloud2 to numpy and o3d
+      points = np.frombuffer(data.data, dtype=np.float32).reshape(-1, 3)# [:, :3]
+      for i in range(len(points)):
+          points[i] = [points[i][2], -points[i][1], points[i][0]]
+      o3dpc = o3d.geometry.PointCloud()
+      o3dpc.points = o3d.utility.Vector3dVector(points)
 
-    # Rotate and translate for position
-    position = np.array([position.x, position.y, position.z], dtype=np.float64)
-    orientation = np.array([orientation.w, orientation.x, orientation.y, orientation.z], dtype=np.float64)
-    R = o3d.geometry.get_rotation_matrix_from_quaternion(orientation)
-    o3dpc.rotate(R, center=(0, 0, 0))
-    o3dpc.translate(position)
+      # Rotate and translate for position
+      position = np.array([position.x, position.y, position.z], dtype=np.float64)
+      orientation = np.array([orientation.w, orientation.x, orientation.y, orientation.z], dtype=np.float64)
+      R = o3d.geometry.get_rotation_matrix_from_quaternion(orientation)
+      o3dpc.rotate(R, center=(0, 0, 0))
+      o3dpc.translate(position)
 
-    # Convert back to PointCloud2
-    points = np.asarray(o3dpc.points, dtype=np.float32)
-    data.data = points.tobytes()
-    self.publisher.publish(data)
+      # Convert back to PointCloud2
+      points = np.asarray(o3dpc.points, dtype=np.float32)
+      data.data = points.tobytes()
+      self.publisher.publish(data)
+      self.last_pub_time = current_time
+     
 
 class PoseNode(Node):
   def __init__(self) -> None:
