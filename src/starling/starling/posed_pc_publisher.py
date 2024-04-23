525757e21188b938ee6bc734c17bdf6dfedc6ba9
Ryan Kuederle <ryankuederle@icloud.com>
2024-04-23 16:52:42 -0400

    fast

--- src/starling/starling/posed_pc_publisher.py
+++ src/starling/starling/posed_pc_publisher.py
@@ -31,19 +31,20 @@ class PCNode(Node):
     )
     self.pose_node = pose_node
     self.pub_rate = 2.0 #2 hz
+    self.max_time_dif = 0.02
     self.last_pub_time = self.get_clock().now().nanoseconds
 
     
 
   def callback(self, data):
+    rclpy.spin_once(self.pose_node)
     current_time = self.get_clock().now().nanoseconds
-    if (current_time - self.last_pub_time) >= (1e9 / self.pub_rate):
+    pose = self.pose_node.get_pose()
+    pose_time = pose.header.stamp.nanosec
+    pc_time = data.header.stamp.nanosec
+    time_dif = abs(pose_time - pc_time) / (1e9 * 1.0)
+    if (current_time - self.last_pub_time) >= (1 / self.pub_rate) * 1e9 and time_dif <= self.max_time_dif:
       # Get latest pose data
-      rclpy.spin_once(self.pose_node)
-      pose = self.pose_node.get_pose()
-      pose_time = pose.header.stamp.nanosec
-      pc_time = data.header.stamp.nanosec
-      time_dif = (pose_time - pc_time) / 1000000000.0
       print(time_dif)
       # Get position and orientation from pose
       position = pose.pose.position
