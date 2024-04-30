790c2283dea64d47285b879203b532361a5b8498
Ryan Kuederle <ryan.kuederle@udri.udayton.edu>
2024-04-30 11:24:19 -0400

    Added voxl mpa to launch

--- src/starling/launch/starling_launch.py
+++ src/starling/launch/starling_launch.py
@@ -6,6 +6,7 @@ def generate_launch_description():
     return LaunchDescription(
         [
             Node(package="starling", executable="circle-flight", name="starling"), 
-            Node(package="starling", executable="custom-pc-pub", name="starling")
+            Node(package="starling", executable="custom-pc-pub", name="starling"), 
+            Node(package="voxl_mpa_to_ros2", executable="voxl_mpa_to_ros2_node", name="voxl-mpa")
         ]
     )
