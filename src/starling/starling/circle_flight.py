24e603142e4e042ac3bb7567cbe730dc7520fe66
Ryan Kuederle <ryan.kuederle@udri.udayton.edu>
2024-05-02 13:16:56 -0400

    repeated arm

--- src/starling/starling/circle_flight.py
+++ src/starling/starling/circle_flight.py
@@ -170,6 +170,7 @@ class OffboardFigure8Node(Node):
             self.armed = True
             # self.publish_takeoff_setpoint(0.0, 0.0, self.end_altitude)
             self.start_time = time.time()
+            self.offboard_setpoint_counter
             self.timer = self.create_timer(0.1, self.timer_callback)
         else:
             try:
@@ -223,13 +224,13 @@ class OffboardFigure8Node(Node):
         """Callback function for the timer."""
         self.publish_offboard_control_heartbeat_signal()
 
-        # if self.offboard_setpoint_counter == 10:
-        #     self.engage_offboard_mode()
-        #     self.arm()
-        #     self.armed = True
+        if self.offboard_setpoint_counter == 10:
+            self.engage_offboard_mode()
+            self.arm()
+            self.armed = True
 
-        # if self.offboard_setpoint_counter < 11:
-        #     self.offboard_setpoint_counter += 1
+        if self.offboard_setpoint_counter < 11:
+            self.offboard_setpoint_counter += 1
         
         if self.start_time + 10 > time.time():
             self.publish_takeoff_setpoint(0.0, 0.0, -self.start_altitude)
