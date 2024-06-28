f9f9c8dc8ca1ea965911fdedf2d38a1b8bf9f0e3
Ryan Kuederle <ryan@rkuederle.com>
2024-06-28 15:47:44 -0400

    Added resets on ready

--- src/starling/starling/circle_flight.py
+++ src/starling/starling/circle_flight.py
@@ -157,6 +157,10 @@ class OffboardFigure8Node(Node):
 
     def ready_callback(self, msg):
         self.voxl_reset.reset()
+        # Reset Path and counters
+        self.path = []
+        self.offboard_setpoint_counter = 0
+        self.offboard_arr_counter = 0
         b = Bool(); b.data  = False
         self.scan_start_pub.publish(b)
         self.scan_end_pub.publish(b)
@@ -170,7 +174,6 @@ class OffboardFigure8Node(Node):
             self.armed = True
             # self.publish_takeoff_setpoint(0.0, 0.0, self.end_altitude)
             self.start_time = time.time()
-            self.offboard_setpoint_counter
             self.timer = self.create_timer(0.1, self.timer_callback)
         else:
             try:
