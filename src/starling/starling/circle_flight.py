4e4c916b8d7d6cd2831581c50583f9291abcb5fb
Ryan Kuederle <ryankuederle@icloud.com>
2024-12-02 08:04:12 -0500

    Removed auto QVIO reset from flight control node

--- src/starling/starling/circle_flight.py
+++ src/starling/starling/circle_flight.py
@@ -95,8 +95,8 @@ class OffboardFigure8Node(Node):
 
         self.ready = False
 
-        self.voxl_reset = VOXLQVIOController()
-        self.voxl_reset.reset()
+        # self.voxl_reset = VOXLQVIOController()
+        # self.voxl_reset.reset()
         self.rate = 20
         self.radius = 0.9
         self.cycle_s = 40
@@ -156,7 +156,7 @@ class OffboardFigure8Node(Node):
         self.scan_end_pub.publish(b)
         self.scan_ended = False
         if msg.data:
-            self.voxl_reset.reset()
+            # self.voxl_reset.reset()
             self.reset()
             self.get_logger().info("Recieved ready command.")
             self.create_path()
@@ -242,7 +242,7 @@ class OffboardFigure8Node(Node):
             self.figure8_timer.cancel()
         except: 
             self.get_logger().info("Failed to cancel fig timer.")
-        self.scan_ended = False
+        self.scan_ended = True
         self.path = []
         self.offboard_setpoint_counter = 0
         self.hit_figure_8 = False
