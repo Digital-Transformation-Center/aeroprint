75b8dc7a580022edc303b9d0ff076fc1ab894f43
Ryan Kuederle <ryan@rkuederle.com>
2025-06-25 13:17:00 -0400

    Remove mpa node from launch

--- src/starling/launch/tof.launch.py
+++ src/starling/launch/tof.launch.py
@@ -5,9 +5,6 @@ from launch_ros.actions import Node
 def generate_launch_description():
     return LaunchDescription(
         [
-            Node(package="voxl_mpa_to_ros2",
-                  executable="voxl_mpa_to_ros2_node",
-                    name="voxl_mpa"),
             Node(package="starling", executable="odometry-tf-publisher", name="odometry_tf_pub"), 
             Node(package="starling", executable="point-cloud-transformer", name="point_cloud_transformer"),
             Node(package="starling", executable="static-tof-tf-publisher", name="static_tof_tf_publisher"),
