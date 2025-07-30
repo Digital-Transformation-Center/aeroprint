2c462522f2f6581ae9b79cb02120406a3fed28be
Ryan Kuederle <ryan@rkuederle.com>
2025-07-30 10:53:05 -0400

    Do not cancel the timer in itself.

--- src/starling/starling/flightdefs/flightnode.py
+++ src/starling/starling/flightdefs/flightnode.py
@@ -192,8 +192,6 @@ class FlightNode(Node):
 
         # We will hold position so the drone can settle, then land
         if self.offboard_arr_counter == self.steps + 50:
-            if self.figure8_timer is not None:
-                self.figure8_timer.cancel()
             self.current_system_state = "LANDING"
             self.is_landing = True
             self.get_logger().info("Figure path completed, landing now.")
